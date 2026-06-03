619f0cd2661b89513a57ad6eec1a93317478f565
Enyan Zhang <53198728+Rock-Z@users.noreply.github.com>
2026-06-03 10:24:21 -0700

    Align classNames helper with display class types

--- quartz/util/lang.ts
+++ quartz/util/lang.ts
@@ -1,11 +1,10 @@
+import type { QuartzDisplayClass } from "../components/types"
+
 export function capitalize(s: string): string {
   return s.substring(0, 1).toUpperCase() + s.substring(1)
 }
 
-export function classNames(
-  displayClass?: "mobile-only" | "desktop-only",
-  ...classes: string[]
-): string {
+export function classNames(displayClass?: QuartzDisplayClass, ...classes: string[]): string {
   if (displayClass) {
     classes.push(displayClass)
   }
